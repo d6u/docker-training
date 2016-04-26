4b3c274238ef04b3f0ca15d94d5c7088e0634879
Carson Banov <carson@spanishdict.com>
2016-04-26 14:28:25 -0400

    7-link-containers/ improve server

--- 7-link-containers/index.js
+++ 7-link-containers/index.js
@@ -11,9 +11,19 @@ redis.on('error', console.error);
 
 redis.set('visitor_count', 0);
 
-// Increment counter for every visitor
-redis.incr('visitor_count', function () {
-  redis.get('visitor_count', function (err, result) {
-    console.log(`Hello, you are the #${result} visitor!`);
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+
+  // Increment counter for every visitor
+  redis.incr('visitor_count', function () {
+    redis.get('visitor_count', function (err, result) {
+      res.send(`Hello, you are the #${result} visitor!`);
+    });
   });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
